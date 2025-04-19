770b560bd67b79a12d5a48cae59e6432da6ec131
AldoLunaBueno <albanovaphi7@gmail.com>
2025-04-19 17:34:34 -0500

    Logic of time conversion extracted from steps.py to parsers.py for unit testing

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -1,20 +1,5 @@
 from behave import given, when, then
-import re
-
-# Función para convertir palabras numéricas a números
-def convertir_palabra_a_numero(palabra):
-    try:
-        return int(palabra)
-    except ValueError:
-        numeros = {
-            "cero": 0, "uno": 1, "una": 1, "dos": 2, "tres": 3, "cuatro": 4, "cinco": 5,
-            "seis": 6, "siete": 7, "ocho": 8, "nueve": 9, "diez": 10, "once": 11,
-            "doce": 12, "trece": 13, "catorce": 14, "quince": 15, "dieciséis": 16,
-            "diecisiete": 17, "dieciocho": 18, "diecinueve": 19, "veinte": 20,
-            "treinta": 30, "cuarenta": 40, "cincuenta": 50, "sesenta": 60, "setenta": 70,
-            "ochenta": 80, "noventa": 90, "media": 0.5
-        }
-        return numeros.get(palabra.lower(), 0)
+from src.parsers import time_description_to_hours
 
 @given('que he comido {cukes:d} pepinos')
 def step_given_eaten_cukes(context, cukes):
@@ -22,27 +7,7 @@ def step_given_eaten_cukes(context, cukes):
 
 @when('espero {time_description}')
 def step_when_wait_time_description(context, time_description):
-    time_description = time_description.strip('"').lower()
-    time_description = time_description.replace('y', ' ')
-    time_description = time_description.strip()
-
-    if time_description == 'media hora':
-        total_time_in_hours = 0.5
-    else:
-        pattern = re.compile(r'(?:(\w+)\s*horas?)?\s*(?:(\w+)\s*minutos?)?')
-        match = pattern.match(time_description)
-
-        if match:
-            hours_word = match.group(1) or "0"
-            minutes_word = match.group(2) or "0"
-
-            hours = convertir_palabra_a_numero(hours_word)
-            minutes = convertir_palabra_a_numero(minutes_word)
-
-            total_time_in_hours = hours + (minutes / 60)
-        else:
-            raise ValueError(f"No se pudo interpretar la descripción del tiempo: {time_description}")
-
+    total_time_in_hours = time_description_to_hours(time_description)
     context.belly.esperar(total_time_in_hours)
 
 @then('mi estómago debería gruñir')
