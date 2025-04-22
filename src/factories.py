5e1b209a444054dee77bdd55a916340704083c63
AldoLunaBueno <albanovaphi7@gmail.com>
2025-04-22 17:27:01 -0500

    Set initial structure

---
+++ src/factories.py
@@ -0,0 +1,12 @@
+# src/factories.py
+import factory
+from src.carrito import Producto
+
+
+class ProductoFactory(factory.Factory):
+    class Meta:
+        model = Producto
+
+    nombre = factory.Faker("word")
+    precio = factory.Faker("pyfloat",
+                           left_digits=2, right_digits=2, positive=True)
