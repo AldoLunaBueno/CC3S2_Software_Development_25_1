019aa88b75d7a20ef6530fc7ac4bac2881591c48
AldoLunaBueno <albanovaphi7@gmail.com>
2025-04-22 12:19:09 -0500

    Add mock to before_scenario environment method to apply BDD to avoid dependency of clock service

--- features/environment.py
+++ features/environment.py
@@ -1,4 +1,7 @@
 from src.belly import Belly
+from unittest.mock import Mock
 
 def before_scenario(context, scenario):
-    context.belly = Belly()
+    fake_clock = Mock()
+    fake_clock.return_value = 10000
+    context.belly = Belly(clock_service = fake_clock)
