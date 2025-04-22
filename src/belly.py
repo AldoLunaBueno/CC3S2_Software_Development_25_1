b7e7afc5f6eb7c9db9651dbc83beecf0b7d94285
AldoLunaBueno <albanovaphi7@gmail.com>
2025-04-21 21:59:20 -0500

    Refactor belly.py at esta_gruñendo() method and add minor addiotions to other auxiliar methods, avoiding test fails

--- src/belly.py
+++ src/belly.py
@@ -3,21 +3,21 @@ import time
 class Belly:
     def __init__(self):
         self._pepinos_comidos = 0
-        self.tiempo_esperado = 0
+        self._tiempo_esperado = 0
 
-    def comer(self, pepinos):
+    def comer(self, pepinos: int):
         self._pepinos_comidos += pepinos
 
-    def esperar(self, tiempo_en_horas):
-        self.tiempo_esperado += tiempo_en_horas
+    def esperar(self, tiempo_en_horas: int):
+        self._tiempo_esperado += tiempo_en_horas
 
     def esta_gruñendo(self, stress_test = False):
         # El estómago gruñe si ha esperado al menos 1.5 horas y ha comido más de 10 pepinos
-        if (not stress_test and (self._pepinos_comidos < 0 or self._pepinos_comidos > 100)):
-            raise ValueError
+        if not stress_test and (self._pepinos_comidos < 0 or self._pepinos_comidos > 100):
+            raise ValueError("Invalid number of cukes")
         if stress_test:
             time.sleep(self._pepinos_comidos / 1000)
-        return self.tiempo_esperado >= 1.5 and self._pepinos_comidos > 10
+        return self._tiempo_esperado >= 1.5 and self._pepinos_comidos > 10
     
     def pepinos_comidos(self):
         return self._pepinos_comidos
